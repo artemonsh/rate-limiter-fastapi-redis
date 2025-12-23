cca69629fbf32ee511bc447c46524760a5642a55
Artem Shumeiko <shumeykoartem@mail.ru>
2025-12-23 19:01:20 +0300

    fix: add lua script and comments

--- main.py
+++ main.py
@@ -16,8 +16,25 @@ def get_redis() -> Redis:
 class RateLimiter:
     def __init__(self, redis: Redis):
         self._redis = redis
+        self._lua_sha = None
+        
+    async def _load_script(self):
+        if self._lua_sha is None:
+            script = """
+            redis.call("ZREMRANGEBYSCORE", KEYS[1], 0, ARGV[2])
+            local count = redis.call("ZCARD", KEYS[1])
+            if count >= tonumber(ARGV[3]) then
+                return 1
+            end
+            redis.call("ZADD", KEYS[1], ARGV[1], ARGV[5])
+            redis.call("EXPIRE", KEYS[1], ARGV[4])
+            return 0
+            """
+            self._lua_sha = await self._redis.script_load(script)
 
-    async def is_limited(
+    # ⚠️ Внимание! метод .pipeline() не исполняет операцию атомарно!
+    # Используйте метод `is_limited()`, в котором используется lua скрипт, запускающийся атомарно
+    async def is_limited_video(
             self,
             ip_address: str,
             endpoint: str,
@@ -44,6 +61,36 @@ class RateLimiter:
 
         _, current_count, _, _ = res
         return current_count >= max_requests
+    
+    # ✅ Корректная реализация с lua скриптом
+    async def is_limited(
+            self,
+            ip_address: str,
+            endpoint: str,
+            max_requests: int,
+            window_seconds: int,
+    ) -> bool:
+        await self._load_script()  # Загружаем lua скрипт
+
+        key = f"rate_limiter:{endpoint}:{ip_address}"
+
+        current_ms = int(time() * 1000)
+        window_start_ms = current_ms - window_seconds * 1000
+        member_id = f"{current_ms}-{random.randint(0, 100_000)}"
+
+        # Запускаем lua скрипт
+        result = await self._redis.evalsha(
+            self._lua_sha,
+            1,
+            key,
+            current_ms,
+            window_start_ms,
+            max_requests,
+            window_seconds,
+            member_id,
+        )
+
+        return result == 1
 
 
 @lru_cache
